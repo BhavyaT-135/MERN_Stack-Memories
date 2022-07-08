b6b460678c8287ae2f7dce51277ba92c7ea21983
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-08 15:36:45 +0530

    ⚡️ Connecting with the database using mongoose

--- server/index.js
+++ server/index.js
@@ -3,3 +3,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://BhavyaTewari:Bhavya2001@cluster0.fq9qu.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch(err => console.log(err.message));
+
+//https://www.mongodb.com/cloud/atlas
