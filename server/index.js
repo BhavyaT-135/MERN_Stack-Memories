f74bc80eca0eb04d978e2226086b84878eac4f90
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-08 14:12:15 +0530

    🎉 Initial commit with client and server folders

---
+++ server/index.js
@@ -0,0 +1,5 @@
+//In the newest versions of node we can use the import syntax by specifying the type as module in package.json
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
