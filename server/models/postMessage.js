d67786b39ce8830b1cd72127427de03a5e92f2dc
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-08 17:45:21 +0530

    ⚡️ Structuring and adding postSchema

---
+++ server/models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+//A mongoose schema is a blueprint for the data that will be stored in the database.
+const postSchema = mongooe.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    }
+});
+
+//Converting postSchema to a model
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import {getPosts} from '../controllers/posts.js';
 
 const router = express.Router();
 
 //Reached by going to localhost:5000/posts
-router.get('/', (req, res) => {
-    res.send('THIS WORKS!');
-})
+router.get('/', getPosts);
 
 export default router;
