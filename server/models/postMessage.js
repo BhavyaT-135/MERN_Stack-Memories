610671cec0af793498f5a3b81068c2442f4fa83b
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-08 17:58:10 +0530

    ⚡️ get and post requests for Post Router

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 //A mongoose schema is a blueprint for the data that will be stored in the database.
-const postSchema = mongooe.Schema({
+const postSchema = mongoose.Schema({
     title: String,
     message: String,
     creator: String,
