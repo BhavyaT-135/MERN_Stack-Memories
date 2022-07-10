d9e2dd970e5c209854698135d8ff6970441992dc
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-11 00:55:02 +0530

    💄 Added styling for Form.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
+
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form"; 
-    
 import memories from "./images/memories.png";
+import useStyles from "./styles";
 
 const App = () => { 
+    const classes = useStyles();
     return (
-        <Container>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className = {classes.heading} variant="h2" align="center">
                     Memories
                 </Typography>
-                <img src = {memories} alt="memories" height="60" width="60" />
+                <img className={classes.image} src = {memories} alt="memories" height="60" width="60" />
             </AppBar>
             <Grow in>
                 <Container>
