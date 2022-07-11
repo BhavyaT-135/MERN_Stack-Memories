ade02609fe33eeef49e96544135eb8566152a4aa
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-11 22:38:20 +0530

    ⚡️ Dispatching the posts action

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import useStyles from './styles.js'
+import { useSelector } from 'react-redux';
 
-const Post = () => {
+import Post from './Post/Post.js'
+
+const Posts = () => {
+  const posts = useSelector((state) => state.posts)
   const classes = useStyles();
+  
+  console.log(posts)
+  
   return (
     <h1>POSTS!</h1>
   )
 }
 
-export default Post;
+export default Posts;
