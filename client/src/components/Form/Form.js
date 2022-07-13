cae5f2d39ded6fa72bf651c55a22048eccffc970
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-13 22:40:03 +0530

    ⚡️ Adding form for posting data

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+
 import useStyles from './styles'
 
 const Form = () => {
   const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <h1>FORM!</h1>
+    <Paper className = {classes.paper}>
+      <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+        
+    </form>
+    </Paper>
   )
 }
 
