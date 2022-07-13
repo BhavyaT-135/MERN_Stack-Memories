fa6666819d6c8b8e2a285a44775ff6c2e4cd916e
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-13 22:35:36 +0530

    🐛 Bug fix in server

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://BhavyaTewari:Bhavya2001@cluster0.fq9qu.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
